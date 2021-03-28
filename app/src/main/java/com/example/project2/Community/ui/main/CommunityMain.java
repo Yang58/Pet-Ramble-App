ead0b5d51e215bbffdb0bd0854d16fb13a234615
yujk745 <yujk745@gmail.com>
2021-03-29 01:50:34 +0900

    DB연동, 내려서 새로고침 추가
    SwipeRefreshLayout으로 내려서 새로고침 가능하게 변경, DB 연동하여 유저 정보와 이름 가져옴

--- app/src/main/java/com/example/project2/Community/ui/main/CommunityMain.java
+++ app/src/main/java/com/example/project2/Community/ui/main/CommunityMain.java
@@ -2,13 +2,16 @@ package com.example.project2.Community.ui.main;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,15 +20,28 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.example.project2.Community.DB.uploadData;
 import com.example.project2.R;
 import com.example.project2.Community.listView.listViewAdapter;
 import com.example.project2.Community.listView.listViewClass;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.sql.Timestamp;
+
+import javax.security.auth.callback.Callback;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -45,6 +61,10 @@ public class CommunityMain extends Fragment {
     private String mParam2;
     private listViewAdapter adapter;
     private ViewGroup vg;
+
+    public interface Callback{
+        void run();
+    }
 
     public CommunityMain() {
         // Required empty public constructor
@@ -84,27 +104,39 @@ public class CommunityMain extends Fragment {
         final View v = inflater.inflate(R.layout.fragment_community_main, container, false);
         vg = container;
         final Context c = vg.getContext();
+        Callback callback;
 
-        final String UID="test";
+        //새로고침 레이아웃
+        SwipeRefreshLayout refreshLayout =(SwipeRefreshLayout) v.findViewById(R.id.cm_main_container_refresh);
+
+        //유저설정
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        //레이아웃 뷰 가져오기
+        TextView dpId = v.findViewById(R.id.cm_main_txt_id);
+        TextView dpName = v.findViewById(R.id.cm_main_txt_name);
+        ImageView dpCover = v.findViewById(R.id.cm_main_img_cover_picture);
+        //이름 가져오기
+        final String[] userName = {"a"};
+        //onComplete 리스너 없이 통신하면 통신상태가 느릴 경우 null이 출력될 수도 있음
+        db.collection("users").document(user.getUid()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                userName[0] = task.getResult().getString("name");
+                dpName.setText(userName[0]);
+                dpId.setText("@"+user.getUid());
+            }
+        });
+
 
         adapter = new listViewAdapter();
         final ListView lv = v.findViewById(R.id.listViewItemContainer);
         //lv.setDivider(null); <-이거로 글과 글 사이에 나누는 선 설정가능.
         lv.setAdapter(adapter);
-        adapter.addItem("@test_account", "아무개", "1가나다라마바사아자차카타파하","");
-        //adapter.addItem("@recipes4dev", "recipes4dev", "일반적으로 안드로이드 앱에 사용하는 ListView는 단순히 문자열만 표시하기보단 이미지나 버튼 또는 크기가 다른 문자열 등으로 구성하는 경우가 더 많습니다." +
-        //        " 단순 문자열만이 아닌 여러 종류의 위젯을 하나의 아이템으로 구성한 ListView를 Custom ListView라고 하며, 안드로이드 앱을 만들 때 가장 많이 사용하는 컴포넌트 중 하나입니다.","");
-        adapter.addItem("@test_account", "아무개", "3가나다라마바사아자차카타파하", "");
-        adapter.addItem("@test_account", "아무개", "4가나다라마바사아자차카타파하", "");
-        adapter.addItem("@test_account", "아무개", "5가나다라마바사아자차카타파하", "");
-        adapter.addItem("@test_account", "아무개", "6가나다라마바사아자차카타파하", "");
-        adapter.addItem("@test_account", "아무개", "7가나다라마바사아자차카타파하", "");
-        adapter.addItem("@test_account", "아무개", "8가나다라마바사아자차카타파하","");
-        adapter.addItem("@test_account", "아무개", "9가나다라마바사아자차카타파하", "");
-        adapter.addItem("@test_account", "아무개", "0가나다라마바사아자차카타파하", "");
-        adapter.addItem("@test_account", "아무개", "가나다라마바사아자차카타파하", "");
-        adapter.setReverse(true);
-        adapter.notifyDataSetChanged();
+        updateList(v,adapter,user);
+
+        chkContentCount(v);
 
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -113,32 +145,36 @@ public class CommunityMain extends Fragment {
             }
         });
 
+        //리스트 최상단에 올라와 있을 때만 새로고침 가능하게 설정
+        lv.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if(firstVisibleItem!=0){
+                    refreshLayout.setEnabled(false);
+                }else{
+                    refreshLayout.setEnabled(true);
+                }
+            }
+        });
+
         Button sndBtn = v.findViewById(R.id.sendButton);
         sndBtn.setOnClickListener(new View.OnClickListener(){
         TextView tField = v.findViewById(R.id.naeyongField);
             @Override
-            public void onClick(View v) {
+            public void onClick(View vv) {
                 if(tField.getText().toString().length()!=0) {
-                    //FirebaseStorage storage = FirebaseStorage.getInstance();
-                    //StorageReference storageRef = storage.getReference();
-                    //StorageReference sr = storageRef.child("KakaoTalk_20181002_210657248.png");
-                    DocumentReference db_access = db.collection("user").document(UID);
-                    //int cover_picture = getResources().getIdentifier(userInfo.getString("cover_photo"),"drawable", c.getPackageName());
-
-                    db_access.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                            DocumentSnapshot d = task.getResult();
-                            if(task.isSuccessful()&&d!=null){
-                                adapter.addItem(0,"@"+d.getString("id"), d.getString("name"), tField.getText().toString(), "");
+                                uploadData upData = new uploadData(tField.getText().toString(),new Timestamp(System.currentTimeMillis()));
+                                uploadContent(user,upData);
+                                updateList(v,adapter,user);
                                 adapter.notifyDataSetChanged();
                                 tField.setText("");
                                 lv.requestFocusFromTouch();
                                 lv.clearFocus();
-                            }
-                        }
-                    });
-                    //adapter.addItem(0,userInfo.getString("id"), userInfo.getString("name"), tField.getText().toString(), sr.toString());
                 }else{
                     Animation shake = AnimationUtils.loadAnimation(getContext(), R.anim.shake);
                     tField.startAnimation(shake);
@@ -146,7 +182,76 @@ public class CommunityMain extends Fragment {
                 }
             }
         });
+
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                updateList(v,adapter,user);
+                chkContentCount(v);
+            }
+        });
+
         return v;
+    }
+
+    public void updateList(View v, listViewAdapter adapter, FirebaseUser user){
+        ListView lv = v.findViewById(R.id.listViewItemContainer);
+        lv.setAdapter(adapter);
+        adapter.removeAllItem();
+        //이름 가져오기
+        final String[] userName = {"a"};
+        //onComplete 리스너 없이 통신하면 통신상태가 느릴 경우 null이 출력될 수도 있음
+        db.collection("users").document(user.getUid()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                userName[0] = task.getResult().getString("name");
+            }
+        });
+        db.collection("community").document(user.getUid()).collection("article").get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for(QueryDocumentSnapshot d : task.getResult()){
+                                Log.wtf(d.getId(),"가져오기 성공");
+                                adapter.addItem("@"+user.getUid(),userName[0],d.getString("content"),"",d.getTimestamp("uptime"));
+                            }
+                            SwipeRefreshLayout refreshLayout = v.findViewById(R.id.cm_main_container_refresh);
+                            refreshLayout.setRefreshing(false);
+                            adapter.sortItems();
+                            adapter.setReverse(true);
+                            adapter.notifyDataSetChanged();
+                        }
+                    }
+                });
+    }
+
+    public void uploadContent(FirebaseUser user, uploadData upData){
+        CollectionReference addDB = db.collection("community").document(user.getUid()).collection("article");
+        addDB.add(upData)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.wtf("경고", "정상처리됨");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.wtf("경고",e.getMessage());
+                    }
+                });
+    }
+
+    public void chkContentCount(View v){
+        //글이 하나도 없을 경우
+        TextView noContent = v.findViewById(R.id.cm_main_txt_noContent);
+        Log.wtf("어댑터",adapter.getCount()+"");
+        if(adapter.getCount()<=0){
+            noContent.setVisibility(View.VISIBLE);
+        }else{
+            noContent.setVisibility(View.GONE);
+        }
     }
 
     public void showDetail(int position) {

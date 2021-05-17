64c198c11ec596cdbc28f392304ac6892aaf1b29
Yang58 <65744610+Yang58@users.noreply.github.com>
2021-05-17 20:23:48 +0900

    산책 종료 시 누적 시간이 60분이 넘어가면 1시간으로 변경 후 저장
    산책 거리 m -> Km 수정
    
    건강 관리 추가 (추천 산책 시간은 추후 추가예정)

--- app/src/main/java/com/example/project2/Friend/FriendChatFragment.java
+++ app/src/main/java/com/example/project2/Friend/FriendChatFragment.java
@@ -1,6 +1,5 @@
 package com.example.project2.Friend;
 
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,34 +19,21 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentResultListener;
 
 import com.bumptech.glide.Glide;
-import com.example.project2.Community.DB.user;
 import com.example.project2.FirebaseDB.ChatDTO;
 import com.example.project2.R;
-
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserInfo;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
-import org.w3c.dom.Text;
-
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.Date;
 
 
 public class FriendChatFragment extends Fragment {
@@ -114,7 +100,9 @@ public class FriendChatFragment extends Fragment {
                 CHAT_NAME = ROOM_ID;
                 USER_NAME = user.getEmail();
 
-                openChat(CHAT_NAME);
+//                openChat(CHAT_NAME);
+
+
                 // 메시지 전송 버튼에 대한 클릭 리스너 지정
                 chat_send.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -150,35 +138,35 @@ public class FriendChatFragment extends Fragment {
         adapter.remove(sdf.format(time)+" " + chatDTO.getUserName() + " : " + chatDTO.getMessage());
     }
 
-    private void openChat(String chatName) {
-        // 리스트 어댑터 생성 및 세팅
-        final ArrayAdapter<String> adapter
-                = new ArrayAdapter<String>(getActivity(), R.layout.listview_chat, R.id.chat_text);
-        chat_view.setAdapter(adapter);
-
-        // 데이터 받아오기 및 어댑터 데이터 추가 및 삭제 등..리스너 관리
-        databaseReference.child("friendchat").child(chatName).addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                addMessage(dataSnapshot, adapter);
-                Log.e("LOG", "s:"+s);
-            }
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-            }
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-                removeMessage(dataSnapshot, adapter);
-            }
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-            }
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-    }
+//    private void openChat(String chatName) {
+//        // 리스트 어댑터 생성 및 세팅
+//        final ArrayAdapter<String> adapter
+//                = new ArrayAdapter<String>(getActivity(), R.id.listview_chat, R.id.chat_text);
+//        chat_view.setAdapter(adapter);
+//
+//        // 데이터 받아오기 및 어댑터 데이터 추가 및 삭제 등..리스너 관리
+//        databaseReference.child("friendchat").child(chatName).addChildEventListener(new ChildEventListener() {
+//            @Override
+//            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+//                addMessage(dataSnapshot, adapter);
+//                Log.e("LOG", "s:"+s);
+//            }
+//            @Override
+//            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+//            }
+//            @Override
+//            public void onChildRemoved(DataSnapshot dataSnapshot) {
+//                removeMessage(dataSnapshot, adapter);
+//            }
+//            @Override
+//            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+//            }
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
+//    }
 
 }
 
